23db51c7d005d6f590e28a583aa0f24e7e516356
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-11-28 12:06:13 +0700

    feat: Config tailwindcss and materialui

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "./component/header";
 
 const LayoutComponent = () => {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   );
