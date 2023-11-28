ae8c1a978fc806a26c5dd9af021a6db91778b005
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-11-28 15:32:40 +0700

    feat: creaate header component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "./component/header";
+import { Box } from "@mui/material";
 
 const LayoutComponent = () => {
   return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
+    <Box className="w-full flex justify-center items-center">
+      <Box className="w-5/6 lg:w-4/6">
+        <Header />
+        <Outlet />
+      </Box>
+    </Box>
   );
 };
 
