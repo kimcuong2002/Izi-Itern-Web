5097d78a80dee4a27ecadd8be959461c7029044c
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-05 08:56:10 +0700

    feat: update router

--- src/routes/render-router.tsx
+++ src/routes/render-router.tsx
@@ -1,6 +1,12 @@
 import { useRoutes } from "react-router-dom";
 import LayoutComponent from "../layout/index";
-import Home from "../pages/home";
+import AllProducts from "../pages/all-product";
+import ProductDetail from "../pages/product-detail";
+import Home from "../pages/home-page";
+import Cart from "../pages/cart";
+import Sofa from "../pages/sofa";
+import Table from "../pages/table";
+import Chair from "../pages/chair";
 
 const routes = [
   {
@@ -10,6 +16,30 @@ const routes = [
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "all-product",
+        element: <AllProducts />,
+      },
+      {
+        path: "/sofa",
+        element: <Sofa />,
+      },
+      {
+        path: "/table",
+        element: <Table />,
+      },
+      {
+        path: "Chair",
+        element: <Chair />,
+      },
+      {
+        path: "/shop/:id",
+        element: <ProductDetail />,
       },
     ],
   },
