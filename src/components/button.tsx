0068d75d4e463ce3fb9e5371cc71523218c56f9e
kimcuong <nguyenhoangkimcuong02@gmail.com>
2023-12-05 08:22:10 +0700

    feat: create Butto component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,11 +2,14 @@ import { FC } from "react";
 
 type Props = {
   buttonName?: string;
+  onClick?: () => void;
 };
 
-const Button: FC<Props> = ({ buttonName }) => {
+const Button: FC<Props> = ({ buttonName, onClick }) => {
   return (
-    <button className="text-sm lg:text-base !text-bold border-2 border-[#78909C] py-3 px-10 text-[#78909C] hover:bg-[#78909C] hover:text-white">
+    <button
+      className="text-sm lg:text-base !text-bold border-2 border-[#78909C] py-3 px-10 text-[#78909C] hover:bg-[#78909C] hover:text-white"
+      onClick={onClick}>
       {buttonName}
     </button>
   );
